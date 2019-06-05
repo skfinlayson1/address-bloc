bf97cee4fbad7d0116aeffb737bf4d92a673dfcb
Shane Finlayson <shane_finlayson@yahoo.com>
2019-06-04 19:28:09 -0600

    Creating Contacts and Connecting: Completed

--- controllers/menuController.js
+++ controllers/menuController.js
@@ -1,4 +1,5 @@
 const inquirer = require('inquirer');
+const ContactController = require('./ContactController.js');
 
 module.exports = class MenuController {
   constructor() {
@@ -14,7 +15,8 @@ module.exports = class MenuController {
           ]
         }
       ];
-      this.contacts = [];
+
+      this.book = new ContactController();
   }
 
   main() {
@@ -46,8 +48,15 @@ module.exports = class MenuController {
 
   addContact() {
     this.clear();
-    console.log('addContact called');
-    this.main();
+    inquirer.prompt(this.book.addContactQuestions).then((answers) => {
+      this.book.addContact(answers.name, answers.phone).then((contact) => {
+        console.log("Contact added successfully!");
+        this.main();
+      }).catch((err) => {
+        console.log(err);
+        this.main();
+      });
+    });
   }
 
   getDate() {
@@ -58,7 +67,7 @@ module.exports = class MenuController {
   }
 
   getContactCount() {
-    return this.contacts.length;
+    return this.book.contacts.length;
   }
 
   remindMe() {
